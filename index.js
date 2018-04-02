ab806571ef53da2c8d53421cb0ad52029f5672b5
theja-vanka <theja.vanka@gmail.com>
2018-04-02 18:38:25 +0530

    initial jquery

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
+$(document).ready(function() {
+	var krishna = new swiperobin();
+	krishna.init();
+})
+
+console.log('function start');
+
+var swiperobin = function() {
+     	this.totalitems = $(".basecard").find('.mycard').length;
+     	this.check = $(".basecard").parents(".container-fluid");
+     	this.wdt = $(this.check).width();
+     	this.hgt = $(this.check).height();
+     	console.log('Object created');
+}
+
+swiperobin.prototype.init = function() {
+	console.log(this.totalitems);
+}
 
 
-//ready function
-$(function() {
-
-
-});
+    /* var totalitems = $(".basecard").find('.mycard').length;
+     var check = $(".basecard").parents(".container-fluid");
+     var wdt = $(check).width();
+     var hgt = $(check).height();
+     console.log('width : ' +wdt+ ' height : '+hgt);*/ 
