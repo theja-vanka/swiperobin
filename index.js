47286723b9e3042b972ca7236e8bbd3e585e653b
theja-vanka <theja.vanka@gmail.com>
2018-04-03 11:24:35 +0530

    defaults data objects

--- index.js
+++ index.js
@@ -1,25 +1,55 @@
 $(document).ready(function() {
 	var krishna = new swiperobin();
 	krishna.init();
-})
+	console.log();
+});
 
-console.log('function start');
 
 var swiperobin = function() {
-     	this.totalitems = $(".basecard").find('.mycard').length;
-     	this.check = $(".basecard").parents(".container-fluid");
-     	this.wdt = $(this.check).width();
-     	this.hgt = $(this.check).height();
-     	console.log('Object created');
+	 data = {
+		totalitems : $(".basecard").find('.mycard').length,
+     	container : $(".basecard").parents(".container-fluid"),
+     	cWidth : $(this.container).width(),
+     	cHeight : $(this.container).height(),
+	};
+	//console.log('Object created');
 }
 
 swiperobin.prototype.init = function() {
-	console.log(this.totalitems);
+	console.log(data.totalitems);
+	this.setOriginalItemDimensions();
 }
 
+swiperobin.prototype.setOriginalItemDimensions = function() {
+	data.container.find('div.mycard').each(function() {
+		if($(this).data('original_width') == undefined || defaults.forcedImageWidth > 0)
+		{
+			$(this).data('original_width', $(this).width());
+		}
+		if($(this).data('original_height') == undefined || defaults.forcedImageHeight > 0) 
+		{
+            $(this).data('original_height', $(this).height());
+        }
+	});
+}
 
-    /* var totalitems = $(".basecard").find('.mycard').length;
-     var check = $(".basecard").parents(".container-fluid");
-     var wdt = $(check).width();
-     var hgt = $(check).height();
-     console.log('width : ' +wdt+ ' height : '+hgt);*/ 
+swiperobin.prototype.defaults = {
+	
+	// appearance defaults
+	seperation: 0,
+	seperationMultiplier: 100,
+	horizontalOffset: 0,
+	horizontalOffsetMultiplier: 1,
+	sizeMultiplier: 0.8,
+	opacityMultiplier: 0.9,
+	flankingItems: 3,
+	//animation defaults
+	speed: 500,
+	animationEasing: 'linear',
+	quickerForFurther: true,
+	orientation: 'horizontal',
+	activeClassName: 'active',
+	forcedImageWidth: 152,
+	forcedImageHeight: 206,
+}
+
