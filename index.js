06775f01698e4a78ed052133e1cefb6d5f72cf2d
theja-vanka <theja.vanka@gmail.com>
2018-04-10 12:38:27 +0530

    Clean v1

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
     var krishna = new swiperobin();
     krishna.init();
-    //console.log(krishna.defaults.forcedImageWidth);
 });
 
 
@@ -36,11 +35,11 @@ var swiperobin = function() {
         sizeMultiplier: 0.8,
         opacityInitial: 1,
         opacityDifference: 0.1,
-        //perspective: 3000, //in pixels
-        perspective: 0, //in pixels
+        perspective: 3000, //in pixels
         flankingItems: 3,
+       
         //animation defaults
-        speed: 500,
+        speed: 700,
         animationEasing: 'linear',
         quickerForFurther: true,
         orientation: 'horizontal',
@@ -48,8 +47,6 @@ var swiperobin = function() {
         forcedImageWidth: 152,
         forcedImageHeight: 206,
     }
-    //console.log('Object created');
-    //console.log(defaults.forcedImageWidth);
 }
 
 swiperobin.prototype.init = function() {
@@ -57,7 +54,6 @@ swiperobin.prototype.init = function() {
     this.preCalculatePositionProperties();
     this.setupRobin();
     this.HandleClicks();
-    //this.locatePosition();
     this.reSize();
 }
 
@@ -66,7 +62,6 @@ swiperobin.prototype.setOriginalItemDimensions = function() {
     this.data.items = this.data.container.find('div.mycard');
     this.data.totalItems = this.data.items.length;
 
-    //console.log('setting dimenstions');
     this.data.items.each(function() {
         if ($(this).attr('original_width') == undefined) 
         {
@@ -178,10 +173,8 @@ swiperobin.prototype.preCalculatePositionProperties = function() {
     }
     
     this.data.maxDistance = 100 + (seperation * 2);
-    console.log(this.data.maxDistance);
-    console.log("MaxWidth= " + this.data.maxDistance * 152 / 100);
-    //console.log(index);
-   //this.shallowCopy();
+    //console.log(this.data.maxDistance);
+    //console.log("MaxWidth= " + this.data.maxDistance * 152 / 100);
 }
 
 swiperobin.prototype.setupRobin = function(subsequent) {
@@ -197,7 +190,7 @@ swiperobin.prototype.setupRobin = function(subsequent) {
     }
     for(var i = 0; i < this.data.totalItems; i++)
     {
-        console.log(this.data.orientation[i], i, this.data.calculations[this.data.orientation[i]]);
+        //console.log(this.data.orientation[i], i, this.data.calculations[this.data.orientation[i]]);
         $(this.data.items[this.data.orientation[i]]).transition({
             position: 'absolute',
             height: 'inherit',
@@ -206,7 +199,7 @@ swiperobin.prototype.setupRobin = function(subsequent) {
             scale: this.data.calculations[i].scale,
             zIndex: this.data.calculations[i].zindex,
             opacity: this.data.calculations[i].opacity,
-        }, 1000);
+        }, this.defaults.speed);
     }
 }
 
@@ -248,122 +241,9 @@ swiperobin.prototype.HandleClicks = function()
 
 
 swiperobin.prototype.reSize = function() {
-    //var pos = this.findKey(index, 3);
 
-    //console.log(pos);
     $(window).resize(function() {
         console.log($(window).width()); // New height
-        //console.log('resize called');
 
     });
-}
-
-
-swiperobin.prototype.shallowCopy = function() {
-
-    //console.log(data.itemsContainer.find('.mycard'));
-    var point = 0;
-    for (var i = this.keyMin(); i <= this.keyMax(); i++) {
-        data.temp = this.findKey(index, i);
-        data.shallowCopyObject[point] = data.itemsContainer.find('.mycard').eq(data.temp);
-        data.shallowCalculations[point++] = data.calculations[data.temp];
-    }
-    //console.log(data.itemsContainer.find('.mycard'));
-    //console.log(data.calculations);
-    //console.log(data.shallowCalculations[1].distance);
-    //console.log(data.shallowCenter);
-}
-
-
-
-
-swiperobin.prototype.locatePosition = function() {
-    var mycard = document.querySelectorAll("div.basecard .mycard");
-    for (var i = 0; i < data.totalItems; i++) {
-        (function(posi) {
-            mycard[i].onclick = function() {
-                data.currentPosition = posi;
-                swiperobin.prototype.rotateRobin();
-            }
-        })(i);
-    }
-    //console.log(index);
-}
-
-swiperobin.prototype.rotateRobin = function() {
-    data.difference = index[data.currentPosition];
-    this.animateRobin();
-}
-
-swiperobin.prototype.animateRobin = function() {
-    this.indexShift();
-    var i = 0;
-    data.shallowCenter = (data.totalItems + data.shallowCenter - data.difference) % data.totalItems;
-    console.log(data.shallowCenter);
-    for (var x in data.shallowCopyObject) {
-        i = (data.totalItems + data.shallowCenter + parseInt(x)) % data.totalItems;
-        data.shallowCopyObject[x].transition({
-            left: data.shallowCalculations[i].distance,
-            position: 'absolute',
-            height: 'inherit',
-            width: 'inherit',
-            scale: data.shallowCalculations[i].scale,
-            zIndex: data.shallowCalculations[i].zindex,
-            opacity: data.shallowCalculations[i].opacity,
-        }, defaults.speed, defaults.animationEasing);
-
-    }
-
-}
-
-swiperobin.prototype.indexShift = function() {
-    var min = this.keyMin();
-    var max = this.keyMax();
-    for (var i = 0; i < data.totalItems; i++) {
-        index[i] = index[i] - data.difference;
-        if (index[i] < min) {
-            index[i] = index[i] + 1;
-            index[i] = index[i] * -1;
-        }
-        if (index[i] > max) {
-            index[i] = index[i] - 1;
-            index[i] = index[i] * -1;
-        }
-    }
-}
-
-
-
-
-swiperobin.prototype.findKey = function(obj, value) {
-    var key = null;
-    for (var prop in obj) {
-        if (obj.hasOwnProperty(prop)) {
-            if (obj[prop] === value) {
-                key = prop;
-            }
-        }
-    }
-    return key;
-}
-
-
-swiperobin.prototype.keyMax = function() {
-    var max = 0;
-    for (var i = 1; i < data.totalItems; i++) {
-        max = Object.keys(index).reduce(function(a, b) {
-            return index[i] > index[max] ? i : max
-        });
-    }
-    return (index[max]);
-}
-//Function Min
-swiperobin.prototype.keyMin = function() {
-    var min = 0;
-    for (var i = 1; i < data.totalItems; i++) {
-        min = Object.keys(index).reduce(function(a, b) {
-            return index[i] < index[min] ? i : min
-        });
-    }
-    return (index[min]);
 }
